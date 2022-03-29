2e19e1768683ef7fc5b9470dcd01b812e1ae070f
Mohammed Elbaz <66201789+mk-elbaz@users.noreply.github.com>
2022-03-29 04:23:18 +0200

    Update settings.py

--- wiki/settings.py
+++ wiki/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-import django_heroku
-django_heroku.settings(locals())
